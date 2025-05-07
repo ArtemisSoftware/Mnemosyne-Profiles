cc9cd8d419674abd20d7a5dae660672ac561c552
gustavo <gustavo.isaac@gmail.com>
2025-05-07 10:55:26 +0100

    Initial commit

---
+++ src/main/kotlin/com/artemissoftware/mnemosyneprofiles/MnemosyneProfilesApplication.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.mnemosyneprofiles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MnemosyneProfilesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MnemosyneProfilesApplication>(*args)
+}
---
+++ src/test/kotlin/com/artemissoftware/mnemosyneprofiles/MnemosyneProfilesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.mnemosyneprofiles
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MnemosyneProfilesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
